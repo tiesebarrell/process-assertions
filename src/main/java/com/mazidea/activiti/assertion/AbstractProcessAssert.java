81d8e20866752aa0e9e76eb68ae36be5a365c050
Tiese Barrell <tiesebarrell@gmail.com>
2013-06-26 22:06:01 +0200

    Refactored log messages to use SLF4J formatting.

--- src/main/java/com/mazidea/activiti/assertion/AbstractProcessAssert.java
+++ src/main/java/com/mazidea/activiti/assertion/AbstractProcessAssert.java
@@ -20,6 +20,8 @@ import java.util.ResourceBundle;
 import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.helpers.FormattingTuple;
+import org.slf4j.helpers.MessageFormatter;
 
 /**
  * Abstract base class for process assertions. Provides easy access to logger
@@ -30,89 +32,92 @@ import org.slf4j.LoggerFactory;
  */
 public abstract class AbstractProcessAssert {
 
-  // Log to the ProcessAssert class' logger
-  private static final Logger LOGGER = LoggerFactory.getLogger(ProcessAssert.class);
+	// Log to the ProcessAssert class' logger
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProcessAssert.class);
 
-  private static ResourceBundle bundle;
+	private static ResourceBundle bundle;
 
-  protected AbstractProcessAssert() {
-    super();
-  }
+	protected AbstractProcessAssert() {
+		super();
+	}
 
-  /**
-   * Logs the message and parameters at trace level if the logger has enabled
-   * ERROR level.
-   * 
-   * @param message
-   *          the log message
-   * @param objects
-   *          the parameters for substitution
-   */
-  protected static void error(final LogMessage message, final Object... objects) {
-    if (LOGGER.isErrorEnabled()) {
-      LOGGER.error(formatLogMessage(message, objects));
-    }
-  }
+	/**
+	 * Logs the message and parameters at trace level.
+	 * 
+	 * @param message
+	 *            the log message
+	 * @param objects
+	 *            the parameters for substitution
+	 */
+	protected static void error(final LogMessage message, final Object... objects) {
+		LOGGER.error(getMessage(message), objects);
+	}
 
-  /**
-   * Logs the message and parameters at debug level if the logger has enabled
-   * DEBUG level.
-   * 
-   * @param message
-   *          the log message
-   * @param objects
-   *          the parameters for substitution
-   */
-  protected static void debug(final LogMessage message, final Object... objects) {
-    if (LOGGER.isDebugEnabled()) {
-      LOGGER.debug(formatLogMessage(message, objects));
-    }
-  }
+	/**
+	 * Logs the message and parameters at debug level.
+	 * 
+	 * @param message
+	 *            the log message
+	 * @param objects
+	 *            the parameters for substitution
+	 */
+	protected static void debug(final LogMessage message, final Object... objects) {
+		LOGGER.debug(getMessage(message), objects);
+	}
 
-  /**
-   * Logs the message and parameters at trace level if the logger has enabled
-   * TRACE level.
-   * 
-   * @param message
-   *          the log message
-   * @param objects
-   *          the parameters for substitution
-   */
-  protected static void trace(final LogMessage message, final Object... objects) {
-    if (LOGGER.isTraceEnabled()) {
-      LOGGER.trace(formatLogMessage(message, objects));
-    }
-  }
+	/**
+	 * Logs the message and parameters at trace level.
+	 * 
+	 * @param message
+	 *            the log message
+	 * @param objects
+	 *            the parameters for substitution
+	 */
+	protected static void trace(final LogMessage message, final Object... objects) {
+		LOGGER.trace(getMessage(message), objects);
+	}
 
-  /**
-   * Fails the assertions by throwing an AssertionError with the provided
-   * message and parameters.
-   * 
-   * Uses {@link String#format(String, Object...)} to format messages.
-   * 
-   * @param message
-   *          the log message
-   * @param objects
-   *          the parameters for substitution
-   */
-  protected static void fail(final LogMessage message, final Object... objects) {
-    final String formattedMessage = formatLogMessage(message, objects);
-    error(LogMessage.ERROR_ASSERTIONS_1, formattedMessage);
-    Assert.fail(formattedMessage);
-  }
+	/**
+	 * Fails the assertions by throwing an AssertionError with the provided
+	 * message and parameters.
+	 * 
+	 * Uses {@link String#format(String, Object...)} to format messages.
+	 * 
+	 * @param message
+	 *            the log message
+	 * @param objects
+	 *            the parameters for substitution
+	 */
+	protected static void fail(final LogMessage message, final Object... objects) {
+		final String substitutedMessage = getFormattedMessage(message, objects);
+		final String failureMessage = getFormattedMessage(LogMessage.ERROR_ASSERTIONS_1, new String[] { substitutedMessage });
+		LOGGER.error(failureMessage);
+		Assert.fail(failureMessage);
+	}
 
-  private static String formatLogMessage(final LogMessage message, final Object... objects) {
-    return String.format(getMessage(message), objects);
-  }
+	/**
+	 * Gets the formatted message by using SLF4J's implementation to allow for
+	 * the same substitution specification in resource bundles.
+	 * 
+	 * @param message
+	 *            the log message
+	 * @param objects
+	 *            the parameters for substitution
+	 * @return the formatted message
+	 */
+	private static String getFormattedMessage(final LogMessage message, final Object[] objects) {
+		final FormattingTuple formattingTuple = MessageFormatter.arrayFormat(getMessage(message), objects);
+		return formattingTuple.getMessage();
+	}
 
-  private static String getMessage(LogMessage message) {
-    return getBundle().getString(message.getBundleKey());
-  }
+	private static String getMessage(LogMessage message) {
+		return getBundle().getString(message.getBundleKey());
+	}
 
-  private static ResourceBundle getBundle() {
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(Constants.LOG_MESSAGES_BUNDLE_NAME);
-    }
-    return bundle;
-  }
+	private static ResourceBundle getBundle() {
+		if (bundle == null) {
+			bundle = ResourceBundle.getBundle(Constants.LOG_MESSAGES_BUNDLE_NAME);
+		}
+		return bundle;
+	}
 }
