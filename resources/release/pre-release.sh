3d78b3fce6abd5a02f210bbc89220c64542de8f1
Tiese Barrell <tiesebarrell@gmail.com>
2014-09-20 14:38:15 +0200

    Added support for Activiti version 5.16.3.

--- resources/release/pre-release.sh
+++ resources/release/pre-release.sh
@@ -18,15 +18,28 @@ echo "* Running pre-release test profiles    *";
 echo "****************************************";
 echo "";
 
-mvn clean test -P activiti-5.12;
-mvn clean test -P activiti-5.12.1;
-mvn clean test -P activiti-5.13;
-mvn clean test -P activiti-5.14;
-mvn clean test -P activiti-5.15;
-mvn clean test -P activiti-5.15.1;
-mvn clean test -P activiti-5.16;
-mvn clean test -P activiti-5.16.1;
-mvn clean test -P activiti-5.16.2;
+activitiVersions=("5.12" "5.12.1" "5.13" "5.14" "5.15" "5.15.1" "5.16" "5.16.1" "5.16.2" "5.16.3")
+
+for activitiVersion in "${activitiVersions[@]}"
+do
+   	:
+   	echo "****************************************";
+	echo "* Running test profile for ${activitiVersion} *";
+	echo "****************************************";
+	echo "";
+   	mvn clean test -P activiti-${activitiVersion};
+done
+
+#mvn clean test -P activiti-5.12;
+#mvn clean test -P activiti-5.12.1;
+#mvn clean test -P activiti-5.13;
+#mvn clean test -P activiti-5.14;
+#mvn clean test -P activiti-5.15;
+#mvn clean test -P activiti-5.15.1;
+#mvn clean test -P activiti-5.16;
+#mvn clean test -P activiti-5.16.1;
+#mvn clean test -P activiti-5.16.2;
+#mvn clean test -P activiti-5.16.3;
 
 echo "****************************************";
 echo "* Running pre-release deploy           *";
