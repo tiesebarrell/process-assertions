23dfe9fec662e331f2a3a9000cf9ac8121bdcb62
Tiese Barrell <tiesebarrell@gmail.com>
2015-05-12 20:53:49 +0200

    Updated release scripts.

--- resources/scripts/release/pre-release.sh
+++ resources/scripts/release/pre-release.sh
@@ -5,44 +5,20 @@ cd $(dirname $0);
 
 source ../shared/prepare-profile-build.sh;
 
-echo "****************************************";
-echo "* Running pre-release checks           *";
-echo "****************************************";
-echo "";
+prelude="[PRERELEASE]";
 
-if [ -e "pom.xml" ]
-then
-  echo "Running from project root directory";
-else
-  echo "Switching to project root directory";
-  cd ../../;
-fi
+echo "${prelude} Running pre-release build";
 
-echo "****************************************";
-echo "* Running pre-release test profiles    *";
-echo "****************************************";
-echo "";
-
-activitiVersions=("5.12" "5.12.1" "5.13" "5.14" "5.15" "5.15.1" "5.16" "5.16.1" "5.16.2" "5.16.3" "5.16.4")
+echo "${prelude} Running test profiles";
 
 for activitiVersion in "${activitiVersions[@]}"
 do
    	:
-   	echo "****************************************";
-	echo "* Running test profile for ${activitiVersion} *";
-	echo "****************************************";
-	echo "";
+	echo "${prelude} Running test profile for Activiti version ${activitiVersion}";
 	mvn clean test -P activiti-${activitiVersion};
 done
 
-echo "****************************************";
-echo "* Running pre-release deploy           *";
-echo "****************************************";
-echo "";
-
+echo "${prelude} Running deploy";
 mvn clean deploy;
 
-echo "****************************************";
-echo "* Completed pre-release checks         *";
-echo "****************************************";
-echo "";
+echo "${prelude} Completed pre-release build";
--- resources/scripts/release/release.sh
+++ resources/scripts/release/release.sh
@@ -3,42 +3,62 @@
 # Script based on suggestions from: https://gist.github.com/nwinkler/9213085
 # See README.md in this script's folder for attribution and reasoning.
 
-#
-# Change the values below for each release
-#
+#Switch to the script's directory regardless of the path the script was invoked from
+cd $(dirname $0);
+
+source ../shared/prepare-profile-build.sh;
+
+prelude="[RELEASE]";
+
+function performRelease {
+	echo "${prelude} Performing release...";
+
+	echo "${prelude} You provided: '$releaseVersion' -> '$developmentVersion' with '$scmCommentPrefix'";
+	
+	# Create a new release branch from develop
+	git checkout -b release/$releaseVersion develop
+ 
+	# Perform Maven release in batch mode 
+	mvn --batch-mode release:prepare release:perform -DscmCommentPrefix="$scmCommentPrefix" -DreleaseVersion=$releaseVersion -DdevelopmentVersion=$developmentVersion
+ 
+	# Merge the new release + snapshot version commits back into develop
+	git checkout develop
+	git merge --no-ff -m "$scmCommentPrefix Merge release/$releaseVersion into develop" release/$releaseVersion
+
+	# Merge the new release but NOT the snapshot version commits back into develop
+	git checkout master
+	git merge --no-ff -m "$scmCommentPrefix Merge previous version into master to avoid the increased version number" release/$releaseVersion~1
+
+	# Remove the release branch that was created
+	git branch -D release/$releaseVersion
+
+	# Switch back to the develop branch
+	git checkout develop
+
+	# Push changes manually
+	git push --all && git push --tags
+
+	echo "${prelude} Release performed.";
+}
 
 # The version to be released
-releaseVersion=0.6.0
+echo "${prelude} Type the version to release (e.g.: 0.6.0) and press [ENTER]: "
+read releaseVersion;
 
 # The next development version
-developmentVersion=0.6.1-SNAPSHOT
+echo "${prelude} Type the development version to switch to after release (e.g.: 0.6.1-SNAPSHOT) and press [ENTER]: "
+read developmentVersion;
 
 # Release comment prefix
-scmCommentPrefix="issue-xxx: "
+echo "${prelude} Type the release comment prefix for the release (e.g.: issue-18: Release 0.6.0) and press [ENTER]: "
+read scmCommentPrefix;
 
-#
-# No changes required from this point for a new release
-#
- 
-# Create a new release branch from develop
-git checkout -b release/$releaseVersion develop
- 
-# Perform Maven release in batch mode 
-mvn --batch-mode release:prepare release:perform -DscmCommentPrefix="$scmCommentPrefix" -DreleaseVersion=$releaseVersion -DdevelopmentVersion=$developmentVersion
- 
-# Merge the new release + snapshot version commits back into develop
-git checkout develop
-git merge --no-ff -m "$scmCommentPrefix Merge release/$releaseVersion into develop" release/$releaseVersion
-
-# Merge the new release but NOT the snapshot version commits back into develop
-git checkout master
-git merge --no-ff -m "$scmCommentPrefix Merge previous version into master to avoid the increased version number" release/$releaseVersion~1
-
-# Remove the release branch that was created
-git branch -D release/$releaseVersion
-
-# Switch back to the develop branch
-git checkout develop
-
-# Push changes manually
-git push --all && git push --tags
+echo "${prelude} You provided: '$releaseVersion' -> '$developmentVersion' with '$scmCommentPrefix'";
+while true; do
+    read -p "${prelude} Is this correct? [y/n]: " performRelease;
+    case $performRelease in
+        [Yy]* ) performRelease; break;;
+        [Nn]* ) echo "${prelude} Exiting build without performing release."; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
