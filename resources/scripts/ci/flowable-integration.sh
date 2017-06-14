ad4f78d5d9a4b54a5c6f4f96c6ffe9e03ab919fd
Tiese Barrell <tiesebarrell@gmail.com>
2017-06-14 21:10:01 +0200

    Refactoring to separate artifacts.

--- resources/scripts/ci/flowable-integration.sh
+++ resources/scripts/ci/flowable-integration.sh
@@ -3,9 +3,7 @@ echo "${prelude} Running Flowable integration tests";
 
 cd integration/flowable-integration;
 
-declare -a flowableVersions=("5.22.0"
-                "5.23.0"
-                "6.0.0"
+declare -a flowableVersions=("6.0.0"
                 "6.0.1"
                 )
 
