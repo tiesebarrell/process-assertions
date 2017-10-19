e795aaac4152ae5bb6ce3b70e65097077536da66
Tiese Barrell <tiesebarrell@gmail.com>
2017-10-19 08:36:03 +0200

    Added support for Flowable 6.2.0 with new package structures in sources.

---
+++ integration/flowable/flowable-integration-6_2_0-6_x/src/test/java/org/toxos/processassertions/flowable/integration/configuration/FlowableTestConfiguration.java
@@ -0,0 +1,84 @@
+package org.toxos.processassertions.flowable.integration.configuration;
+
+import org.flowable.engine.*;
+import org.flowable.engine.common.impl.history.HistoryLevel;
+import org.flowable.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.flowable.engine.test.FlowableRule;
+import org.flowable.spring.ProcessEngineFactoryBean;
+import org.flowable.spring.SpringProcessEngineConfiguration;
+import org.h2.Driver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlowableTestConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        final SimpleDriverDataSource result = new SimpleDriverDataSource();
+        result.setDriverClass(Driver.class);
+        result.setUrl("jdbc:h2:mem:flowable;DB_CLOSE_DELAY=1000");
+        result.setUsername("sa");
+        result.setPassword("");
+        return result;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    public ProcessEngineConfigurationImpl processEngineConfiguration() {
+        final SpringProcessEngineConfiguration result = new SpringProcessEngineConfiguration();
+        result.setDataSource(dataSource());
+        result.setTransactionManager(transactionManager());
+        result.setDatabaseSchemaUpdate("true");
+        result.setAsyncExecutorActivate(false);
+        result.setHistoryLevel(HistoryLevel.FULL);
+        return result;
+    }
+
+    @Bean
+    public ProcessEngineFactoryBean processEngine() {
+        final ProcessEngineFactoryBean result = new ProcessEngineFactoryBean();
+        result.setProcessEngineConfiguration(processEngineConfiguration());
+        return result;
+    }
+
+    @Bean
+    public RepositoryService repositoryService() throws Exception {
+        return processEngine().getObject().getRepositoryService();
+    }
+
+    @Bean
+    public RuntimeService runtimeService() throws Exception {
+        return processEngine().getObject().getRuntimeService();
+    }
+
+    @Bean
+    public TaskService taskService() throws Exception {
+        return processEngine().getObject().getTaskService();
+    }
+
+    @Bean
+    public HistoryService historyService() throws Exception {
+        return processEngine().getObject().getHistoryService();
+    }
+
+    @Bean
+    public ManagementService managementService() throws Exception {
+        return processEngine().getObject().getManagementService();
+    }
+
+    @Bean
+    public FlowableRule flowableRule() throws Exception {
+        return new FlowableRule(processEngine().getObject());
+    }
+
+}
--- resources/scripts/ci/flowable-integration.sh
+++ resources/scripts/ci/flowable-integration.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "${prelude} Running Flowable integration tests";
 
-cd integration/flowable-integration;
+cd integration/flowable/flowable-integration-6_0_0-6_1_2;
 
 declare -a flowableVersions=(
                 "6.0.0"
@@ -9,6 +9,25 @@ declare -a flowableVersions=(
                 "6.1.0"
                 "6.1.1"
                 "6.1.2"
+                )
+
+echo "${prelude} Supported Flowable versions:";
+for flowableVersion in "${flowableVersions[@]}"
+do
+   	:
+	echo "${prelude}   Flowable version ${flowableVersion}";
+done
+
+for flowableVersion in "${flowableVersions[@]}"
+do
+   	:
+	echo "${prelude} Running test profile for Flowable version ${flowableVersion}";
+	mvn clean dependency:tree test -P flowable-${flowableVersion};
+done
+
+cd ../flowable-integration-6_2_0-6_x;
+
+declare -a flowableVersions=(
                 "6.2.0"
                 )
 
@@ -26,4 +45,4 @@ do
 	mvn clean dependency:tree test -P flowable-${flowableVersion};
 done
 
-cd ../../;
+cd ../../../;
